2bf3178aa23bbbb61e4ebda26d40ad880dbe5677
unknown <bibekrokaya246@gmail.com>
2024-08-31 13:53:12 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const Menu =document.querySelector(".menu");
+    Menu.addEventListener("click", ()=>{
+        const MobileMenu=document.querySelector(".mobile-menu");
+        MobileMenu.classList.toggle("hidden");
+    });
+});
+
+
+/// hero -sec
+
+
